80a58bedab460170f8c962e3f4ad53e285c0973b
Ganesh Bhosale <gdb.sci123@gmail.com>
2015-03-16 13:59:26 +0530

    some more changes

--- drowsyness_detection_2.py
+++ drowsyness_detection_2.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import time
 
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascades2/haarcascade_eye.xml')
 
-simulate_real_time = "false"
+simulate_real_time = "true"
 
 if(simulate_real_time == "true"):
     cap = cv2.VideoCapture(0)
@@ -34,46 +35,58 @@ def detect_and_draw(img, gray):
             
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),1)
             
-            if(simulate_real_time == "false"):
+            roi_eye_gray = roi_gray[ey:ey+eh, ex:ex+ew]
+            roi_eye_color = roi_color[ey:ey+eh, ex:ex+ew]
 
-                roi_eye_gray = roi_gray[ey:ey+eh, ex:ex+ew]
-                roi_eye_color = roi_color[ey:ey+eh, ex:ex+ew]
+            # create & normalize histogram ---------
+            hist = cv2.calcHist([roi_eye_gray], [0],None, [256], [0,256])
+            histn = []
+            max_val = 0
+            for i in hist:
+                value = int(i[0])
+                histn.append(value)
+                if(value > max_val):
+                    max_val = value
+            for index, value in enumerate(histn):
+                histn[index] = ((value * 256) / max_val)
+            print histn
+            # normalize histogram ends ---------
 
-                # create & normalize histogram ---------
-                hist = cv2.calcHist([roi_eye_gray], [0],None, [256], [0,256])
-                histn = []
-                max_val = 0
-                for i in hist:
-                    value = int(i[0])
-                    histn.append(value)
-                    if(value > max_val):
-                        max_val = value
-                for index, value in enumerate(histn):
-                    histn[index] = ((value * 256) / max_val)
-                print histn
-                # normalize histogram ends ---------
+            
 
+            # Slice
+            roi_eye_gray2 = roi_eye_gray.copy()
+            #roi_eye_gray2 = cv2.threshold(roi_eye_gray2, 50, 255, cv2.THRESH_TOZERO)
+            #roi_eye_gray2 = cv2.adaptiveThreshold(roi_eye_gray2, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 10)
+
+            threshold = 65
+            total_white = 0
+            total_black = 0
+            for i in range(0, roi_eye_gray2.shape[0]):
+                for j in range(0, roi_eye_gray2.shape[1]):
+                    pixel_value = roi_eye_gray2[i, j]
+                    if(pixel_value >= threshold):
+                        roi_eye_gray2[i, j] = 255
+                        total_white = total_white + 1
+                    else:
+                        roi_eye_gray2[i, j] = 0
+                        total_black = total_black + 1
+
+            #cv2.imshow(image_name, roi_eye_gray2)
+            print total_black
+            print total_white
+
+            if(simulate_real_time == "true"):
+                pass
+                # put number on image
+                if(cnt_eye == 0):
+                    cv2.putText(img, ""+str(total_white)+", "+str(total_black), (10, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, 255)
+                else:
+                    cv2.putText(img, ""+str(total_white)+", "+str(total_black), (400, 40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, 255)
+            else:
                 # Plot Histogram
                 plt.subplot(2,3,((cnt_eye*3)+1)),plt.hist(roi_eye_gray.ravel(), 256, [0,256])
                 plt.title(image_name+' Hist')
-
-                # Slice
-                roi_eye_gray2 = roi_eye_gray.copy()
-                #roi_eye_gray2 = cv2.threshold(roi_eye_gray2, 50, 255, cv2.THRESH_TOZERO)
-                #roi_eye_gray2 = cv2.adaptiveThreshold(roi_eye_gray2, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 10)
-
-                threshold = 65
-                for i in range(0, roi_eye_gray2.shape[0]):
-                    for j in range(0, roi_eye_gray2.shape[1]):
-                        pixel_value = roi_eye_gray2[i, j]
-                        if(pixel_value >= threshold):
-                            roi_eye_gray2[i, j] = 255
-                        else:
-                            roi_eye_gray2[i, j] = 0
-
-                #cv2.imshow(image_name, roi_eye_gray2)
-
-
                 # Plot Eye Images
                 plt.subplot(2,3,((cnt_eye*3)+2)),plt.imshow(roi_eye_color, 'gray')
                 plt.title(image_name+' Image Threshold')
@@ -81,28 +94,38 @@ def detect_and_draw(img, gray):
                 # Plot Eye Images after threshold
                 plt.subplot(2,3,((cnt_eye*3)+3)),plt.imshow(roi_eye_gray2, 'gray')
                 plt.title(image_name+' Image')
-
-            cnt_eye = cnt_eye + 1
             
+            cnt_eye = cnt_eye + 1
     cv2.imshow('frame', img)
     if(simulate_real_time == "false"):
         plt.show()
-    
+    #img.releaseImage()
     
 
 if __name__ == '__main__':
     if(simulate_real_time == "true"):
+        countQuit = 10
         while(True):
+            #time.sleep(1)
             ret, frame = cap.read()
-            frame = cv2.resize(frame, (600, 350))
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            detect_and_draw(frame, gray)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            if frame != None:
+                frame = cv2.resize(frame, (600, 350))
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                detect_and_draw(frame, gray)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                print "Frame Grabbed Problem";
+                countQuit = countQuit - 1
+                if countQuit <= 0:
+                    break
+                else:
+                    continue
+        cap.release()
     else:
         # Capture frame-by-frame
         frame = cv2.imread('face_img.jpg')
-
+        
         # Resize Image
         frame = cv2.resize(frame, (600, 350))
 
@@ -114,7 +137,5 @@ if __name__ == '__main__':
 
         # Display the resulting frame
         #cv2.imshow('frame',gray)
-
-
-    # When everything done, release the capture
+    
     cv2.destroyAllWindows()
